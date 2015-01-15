a30e41a86fdeeaf7ce94cf0eb9812413504c3dfd
phagocyte <githubphagocyte@gmail.com>
2015-01-15 19:13:24 +0000

    Move constants out of game.py

--- python/game.py
+++ python/game.py
@@ -1,53 +1,4 @@
-from __future__ import print_function
-
-from board import Board
-from random import Random
-from specimen import Specimen
-import player as Player
-import sys
-import trap
-import coordinates
-import time
-import constants
-
-#Pick one of the following:
-#from graphical_display import Display  #Requires pygame
-#from tkinter_display import Display #Requires tkinter
-#from text_display import Display
-from no_display import Display
-
-
-if sys.version_info >= (3,):
-    xrange = range
-
-NUMBER_OF_BOARDS = 1
-BOARD_WIDTH = coordinates.BOARD_WIDTH
-BOARD_HEIGHT = coordinates.BOARD_HEIGHT
-
-NUMBER_OF_SAFE_COLORS = 10
-NUMBER_OF_COLORS = sum([trap_type.max_traps for trap_type in trap.trap_types])\
-                   + NUMBER_OF_SAFE_COLORS
-
-NUMBER_OF_TURNS = 10000
-
-INITIAL_SPECIMENS = 15
-SPECIMEN_LIFESPAN = 100
-REPRODUCTION_RATE = 10
-NUM_PARENTS = 2
-
-DNA_LENGTH = 50
-DNA_MAX_VALUE = (1 << DNA_LENGTH) - 1
-DNA_CROSSOVER_RATE = .1
-DNA_MUTATION_RATE = .01
-
-VISION_WIDTH = 5
-VISION_DISTANCE = int(VISION_WIDTH/2)
-VISION = [[coordinates.Coordinate(x, y)
-          for x in xrange(-VISION_DISTANCE, VISION_DISTANCE+1)]
-          for y in xrange(VISION_DISTANCE, -VISION_DISTANCE-1, -1)]
-
-RANDOM_SEED = 13722829
-
+from constants import *
 
 random = Random(RANDOM_SEED)
 
@@ -77,9 +28,7 @@ def sanitized(board):
             next_squares = set(unvisited)
     return safe_squares
 
-
-
-
+    
 def initialize_board():
     colors = list(range(NUMBER_OF_COLORS))
     random.shuffle(colors)
